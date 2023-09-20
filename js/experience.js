79279dc66fbc4aeaa6264ef879a0a8d632f6a019
Praneeth Shetty <praneethshetty10@gmail.com>
2023-09-20 12:03:41 +0530

    adding experiences

--- js/experience.js
+++ js/experience.js
@@ -1,34 +1,45 @@
-experienceDetails = [
+const experienceDetails = [
     {
-        term: "July 2023 - Sep 2023",
+        term: "Aug 2021 - Present",
+        position: "Founder",
+        companyName: "IODump",
+        companyLocation: "Remote",
+        description: ["Led a team of 20 individuals in various domains including development, event organization, workshops, and technical education, fostering collaboration and achieving successful outcomes.",
+            "Created Personalized Event Websites, resulting in the onboarding of nearly 500 participants and providing them with an engaging experience.",
+            "Developed a CTF cum Quiz platform, hosting a contest for 280+ participants simultaneously, showcasing the ability to handle large-scale events and deliver exceptional user experiences.",
+            "Impacted over 1k+ students by offering valuable resources and support, and also taught programming languages, Data Structures & Algorithms, and technologies like React, Solidity, Git/GitHub to 350+ students, equipping them with essential skills for success in the tech industry."
+        ]
+    },
+    {
+        term: "May 2023 - Sep 2023",
+        position: "GSoC Contributor",
+        companyName: "FURY, Python Software Foundation",
+        companyLocation: "Remote",
+        description: ["Contributed to the FURY library, a scientific visualizer cum game engine used by over 500 users.",
+            "Enhanced the library's user interface (UI) by adding new components that seamlessly integrate with existing elements.",
+            "Developed 4 new components: SpinBoxUI, CardUI, FileDialogUI, and TreeUI, completely with unit tests, ensuring compatibility with existing components.",
+            "Refactored the core TextBlock2D component, resolving alignment and justification issues while introducing automatic font and background scaling.",
+            "Successfully addressed various issues, including ComboBox functionality, Website redirection, and the decision-making process regarding Scrollbar UI.",
+        ]
+    },
+    {
+        term: "Jun 2023 - Oct 2023",
         position: "GSoC Contributor",
         companyName: "FURY, Python Software Foundation",
         companyLocation: "Remote",
         description: ["Contributed to the Python Software Foundation as a GSoC contributor, working on the open-source project called FURY which is used by 500+ peoples.",
             "Developed a highly useful component called DrawPanel, enabling researchers and students to visualize complex geometrical objects and chemistry molecules.",
             "Implemented 8 different modes for creating primitive objects, allowing users to easily manipulate and group/un-group them along with a freehand mode, providing the flexibility to draw custom shapes and designs.",
-            "Significantly improved test coverage by 10% through the creation and updating of unit tests, ensuring the stability and reliability of the software.",
-            "Helped to add 4 new UI components and refactored a core element, which served as the foundation for all other UI components. Additionally, I implemented new features to meet the evolving project requirements.",]
-    },
-    {
-        term: "August 2021 - Present",
-        position: "Builder",
-        companyName: "IODump",
-        companyLocation: "Remote",
-        description: ["Led a team of 20 individuals in various domains including development, event organization, workshops, and technical education, fostering collaboration and achieving successful outcomes.",
-            "Created Personalized Event Websites, resulting in the onboarding of nearly 500 participants and providing them with an engaging experience.",
-            "Developed a CTF cum Quiz platform, hosting a contest for 280+ participants simultaneously, showcasing the ability to handle large-scale events and deliver exceptional user experiences.",
-            "Impacted over 1k+ students by offering valuable resources and support, and also taught programming languages, Data Structures & Algorithms, and technologies like React, Solidity, Git/GitHub to 350+ students, equipping them with    essential skills for success in the tech industry."
-        ]
+            "Significantly improved test coverage by 10% through the creation and updating of unit tests, ensuring the stability and reliability of the software."]
     }
 ]
 
 
-let experienceCard = document.getElementById("experience-card");
+const experienceCard = document.getElementById("experience-card");
 
 for (let i = 0; i < experienceDetails.length; i++) {
 
-    experienceDescription = "";
+    let experienceDescription = "";
     experienceDetails[i].description.forEach(line => {
         experienceDescription += `<p>${line}</p>`;
     });
